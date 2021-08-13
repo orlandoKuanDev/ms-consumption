bf6b9d3b6c4029f659ad514d0ec4234556aebba7
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-08-12 19:37:21 -0500

    Initial commit

---
+++ src/main/java/com/example/msconsumption/MsConsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.msconsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConsumptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConsumptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msconsumption/MsConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msconsumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConsumptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
